9cf4a70c7ac04784f9d4204403cfd3ccca5127bf
Farid Ansari <farid12ansari7@gmail.com>
2017-12-14 00:32:08 +0530

    control buttons and dice image added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Player0 from './components/Player0/player0';
 import Player1 from './components/Player1/player1';
 import Button from './components/Button/button';
+import Image from './img/dice-5.png';
 import './App.css';
 
 class App extends Component {
@@ -15,6 +16,13 @@ class App extends Component {
           iconName="ion-ios-plus-outline"
           name="New Game"
         />
+        <Button btncss="btn-roll" iconName="ion-ios-loop" name="Roll Dice" />
+        <Button
+          btncss="btn-hold"
+          iconName="ion-ios-download-outline"
+          name="Hold"
+        />
+        <img src={Image} alt="Dice" className="dice" />
       </div>
     );
   }
